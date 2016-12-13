11287522e669e04af77d846c40a27031fee09176
zladdi <zladdi@users.noreply.github.com>
2016-12-13 23:54:53 +0100

    Initial Commit

---
+++ diff-doc-osx/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  diff-doc-osx
+//
+//  Created by zlatko on 13/12/16.
+//  Copyright © 2016 Zlatko Franjcic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
